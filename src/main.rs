c7a2a1f428e641e96bed31e197aae05a85776a97
c0utin <rafaelcouto111@gmail.com>
2024-09-30 21:25:22 -0300

    feat: only capture tcp packets

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,32 @@ fn main() -> io::Result<()> {
                 let dest = pkt.destination_addr();
                 let proto = pkt.protocol();
 
-                eprintln!(
-                    "{} -> {} {}b of protocol {:x}",
-                    src,
-                    dest,
-                    pkt.payload_len(),
-                    proto,
-                );
+                if proto != 0x06 {
+                    // TCP only
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + pkt.slice().len()..]){
+                    Ok(pkt) => {
+                        eprintln!(
+                            "{} -> {} {}b of tcp to port {}",
+                            src,
+                            dest,
+                            pkt.slice().len(),
+                            pkt.destination_port(),
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("a vei affs {:?}", e);
+                    }
+                }
+
             }
+
             Err(e) => {
                 eprintln!("a vei affs {:?}", e);
             }
         }
+
     }
 }
